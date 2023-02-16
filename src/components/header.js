deca44e45a66dea6586f2b28f74778c12be85c73
Jasvaran Mann <mannjesse355@gmail.com>
2023-02-16 14:18:08 -0800

    set background and header

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+export default function Header(props){
+
+    return (
+        <div className="Header">
+            <h1>Memory Card Project</h1>
+            <a href="https://www.theodinproject.com">theodinproject</a>
+        </div>
+    )
+}
